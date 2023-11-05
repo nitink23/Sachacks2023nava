e217e259cfb4d88bcc915064a3e424750b6edfc6
AkshajJ <aaj2004@gmail.com>
2023-11-04 17:09:56 -0700

    hi

--- job_scraper/upload_to_mongo.py
+++ job_scraper/upload_to_mongo.py
@@ -2,32 +2,14 @@ import pymongo
 import json
 
 # Replace these with your own MongoDB Atlas connection details
-MONGO_URI = "mongodb+srv://nava:sachacks23@cluster.mongodb.net/test"
+MONGO_URI = "mongodb+srv://nava:sachacks23@cluster0.wdsjs4p.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "nava"
 COLLECTION_NAME = "jd"
 
-with open('technology_keywords.json', "w") as f:
+with open('technology_keywords.json', "r") as f:
     data_to_insert = json.load(f)
-
-# Sample JSON objects (replace these with your own data)
-# data_to_insert = [
-#     {
-#         "name": "John",
-#         "age": 30,
-#         "city": "New York"
-#     },
-#     {
-#         "name": "Alice",
-#         "age": 25,git push
-
-#         "city": "Los Angeles"
-#     },
-#     {
-#         "name": "Bob",
-#         "age": 35,
-#         "city": "San Francisco"
-#     }
-# ]
+    
+    
 
 # Connect to MongoDB Atlas
 client = pymongo.MongoClient(MONGO_URI)
